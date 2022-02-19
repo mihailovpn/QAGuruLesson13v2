4e1b40d25f4dd7ba9c12269b4c3e1fec6057a2d2
Павел Михайлов <pavel.mikhaylov@simbirsoft.com>
2022-02-19 13:02:09 +0300

    refactor all test to Annotated steps

--- src/test/java/sim/mikhailov/tests/MainPageTests.java
+++ src/test/java/sim/mikhailov/tests/MainPageTests.java
@@ -1,7 +1,5 @@
 package sim.mikhailov.tests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,8 +7,7 @@ import org.junit.jupiter.api.Test;
 import sim.mikhailov.tests.pages.LoginPage;
 import sim.mikhailov.tests.pages.MainPage;
 
-import static com.codeborne.selenide.Selenide.$;
-import static io.qameta.allure.Allure.step;
+@Feature("MainPage")
 
 public class MainPageTests extends TestBase {
     MainPage mainPage = new MainPage();
@@ -18,62 +15,40 @@ public class MainPageTests extends TestBase {
 
     @BeforeEach
     void logIn(){
-        step("Log in", () ->
-                loginPage.logIn());
+        loginPage.logIn();
     }
 
     @Test
-    @Feature("MainPage")
     @DisplayName("Check slogan element text")
     void sloganElementTest() {
-        step("Check slogan element text " + loginPage.url, () ->
-                $(mainPage.sloganLocator).
-                    shouldHave(Condition.text(mainPage.sloganText)));
+        mainPage.checkSloganElementText();
     }
+
     @Test
-    @Feature("MainPage")
     @DisplayName("Check My Data section by Default")
     void myDataSectionByDefaultTest() {
-        step("Check My Data section element " + loginPage.url, () ->
-                $(mainPage.commonInfoSelector).
-                    shouldHave(Condition.text(mainPage.commonInfoText)));
+        mainPage.checkCommonInfoText();
     }
+
     @Test
-    @Feature("MainPage")
     @DisplayName("Check My Data section on click")
     void myDataSectionOnClickTest() {
-        step("Click Organisation section element " + loginPage.url, () ->
-                $(mainPage.organisationSelector).click());
-
-        step("Click My Data section element " + loginPage.url, () ->
-                $(mainPage.myDataSelector).click());
-
-        step("Check My Data section element " + loginPage.url, () ->
-                $(mainPage.commonInfoSelector).
-                        shouldHave(Condition.text(mainPage.commonInfoText)));
+        mainPage.clickOrganisationLabel();
+        mainPage.clickMyDataLabel();
+        mainPage.checkCommonInfoText();
     }
 
     @Test
-    @Feature("MainPage")
     @DisplayName("Check Settings section on click")
     void settingsSectionOnClickTest() {
-        step("Click Settings section element " + loginPage.url, () ->
-                $(mainPage.settingsSelector).click());
-
-        step("Check Organisation section element " + loginPage.url, () ->
-                $(mainPage.securitySelector).
-                        shouldHave(Condition.text(mainPage.securityText)));
+        mainPage.clickSettingsLabel();
+        mainPage.checkSecurityText();
     }
 
     @Test
-    @Feature("MainPage")
     @DisplayName("Check Organisation section on click")
     void organisationSectionOnClickTest() {
-        step("Click Organisation section element " + loginPage.url, () ->
-                $(mainPage.organisationSelector).click());
-
-        step("Check Organisation section element " + loginPage.url, () ->
-                $(mainPage.organisationNameSelector).
-                        shouldHave(Condition.text(mainPage.organisationNameText)));
+        mainPage.clickOrganisationLabel();
+        mainPage.checkOrganisationNameText();
     }
 }
--- src/test/java/sim/mikhailov/tests/pages/MainPage.java
+++ src/test/java/sim/mikhailov/tests/pages/MainPage.java
@@ -1,11 +1,16 @@
 package sim.mikhailov.tests.pages;
 
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class MainPage {
     public String userNameLocator = ".user-name";
     public String sloganLocator = ".slogan";
     public String myDataSelector = "nav > a:nth-child(1)";
     public String settingsSelector = "nav > a:nth-child(2)";
-    public String organisationSelector = "nav > a:nth-child(3)";
+    public String organisationSelector = "nav > a:last-child";
     public String commonInfoSelector = "my-common-information h3";
     public String organisationNameSelector = ".scroll-pagination h3";
     public String securitySelector = "h3:first-of-type";
@@ -18,6 +23,46 @@ public class MainPage {
     public String securityText = "Безопасность";
 
 
+
+    @Step("Check successfully Log in")
+    public void checkSuccessfullyLogIn() {
+        $(userNameLocator).shouldHave(Condition.text(userNameText));
+    }
+
+    @Step("Check slogan element text")
+    public void checkSloganElementText() {
+        $(sloganLocator).shouldHave(Condition.text(sloganText));
+    }
+
+    @Step("Check Common Info element text")
+    public void checkCommonInfoText() {
+        $(commonInfoSelector).shouldHave(Condition.text(commonInfoText));
+    }
+
+    @Step("Check security element text")
+    public void checkSecurityText() {
+        $(securitySelector).shouldHave(Condition.text(securityText));
+    }
+
+    @Step("Check organisation name element text")
+    public void checkOrganisationNameText() {
+        $(organisationNameSelector).shouldHave(Condition.text(organisationNameText));
+    }
+
+    @Step("Click Organisation section label")
+    public void clickOrganisationLabel() {
+        $(organisationSelector).click();
+    }
+
+    @Step("Click Ma Data section label")
+    public void clickMyDataLabel() {
+        $(myDataSelector).click();
+    }
+
+    @Step("Click Settings section label")
+    public void clickSettingsLabel() {
+        $(settingsSelector).click();
+    }
 
 
 
